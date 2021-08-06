42aed2c767361f497c8b92b1e5beafbc07d36f64
HCoulson <HCoulson@users.noreply.github.com>
2021-08-06 16:57:45 -0700

    created basic app framework

---
+++ lesson07/assignment/assignment07/src/components/mainheader.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="mainheader">
+    <div class="bg-dark">
+      <div class="w-50 mx-auto pt-5 pb-4">
+        <img alt="Winecierge logo" src="../assets/winecierge-logo-white.svg">
+        <h5 class="mb-0 pb-4 text-white text-center">{{ msg }}</h5>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'mainheader',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
